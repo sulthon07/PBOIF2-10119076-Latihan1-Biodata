3f40e06876e6f7facdcfa2f434e04388c4992a78
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-17 16:53:37 +0700

    PBOIF2-10119076-Latihan1-Biodata

---
+++ src/pboif2/pkg10119076/latihan1/biodata/PBOIF210119076Latihan1Biodata.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan1.biodata;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119076
+ * Deskripsi Program : perintah menampilkan value kelayar
+ */
+public class PBOIF210119076Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+       System.out.println("Nama\t\t: Sulthon Naufal Akmal");
+       System.out.println("ALAMAT\t\t: Jalan Dirgantara 1 No 5");
+       System.out.println("TEMPAT LAHIR\t: Bandung");
+       System.out.println("TANGGAL LAHIR\t: 7 Juli 2001");
+       System.out.println("--------------------");
+       System.out.println("HOBI\t\t: Bermain Game");
+       System.out.println("MAKES\t\t: Nasi Goreng");
+       System.out.println("MIKES\t\t: Air putih teh susu");
+    }
+    
+}
